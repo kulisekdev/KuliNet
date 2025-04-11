70883a25f23d2af0d663f2ab9fdde62e0fc309a9
kulisekdev <kulisek@hnateklukas.xyz>
2025-04-11 23:19:49 +0200

    renamed json from token => configuration

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Imports
+import json
 from discord.ext import commands
 import discord
 
-
-
+# import discord bot token from token.json
+with open("configuration.json", "r") as token_json:
+    load_token = json.load(token_json)
+    token = load_token["Token"]
 
 # Defining bot
+bot = commands.Bot(command_prefix="")
+
+@bot.slash
