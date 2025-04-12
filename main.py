6868c2e73188d15be04992c65465e85390ce7277
kulisekdev <kulisek@hnateklukas.xyz>
2025-04-12 08:10:23 +0200

    Added join command for joining a voice

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 import json
 from discord.ext import commands
 import discord
+import os
+
+#testing...
+from dotenv import load_dotenv
+load_dotenv()
+temptoken = os.getenv("token")
+
+
+
 
 # import discord bot token from token.json
 with open("configuration.json", "r") as token_json:
@@ -9,18 +18,44 @@ with open("configuration.json", "r") as token_json:
     token = load_token["Token"]
     prefix = load_token["command_prefix"]
 
-# Defining bot
-bot = commands.Bot(command_prefix=prefix)
+# Defining bot and stuff
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.command()
 async def join(ctx):
     channel = ctx.author.voice.channel
     joined = discord.Embed(
-        title=f"Joined to {channel.name}",
+        title=f"Joined to **{channel.name}**",
         description="The bot successfully joined the voice channel.",
         timestamp=discord.utils.utcnow(),
         color=discord.Color.green()
     )
-    if ctx.author.voice:
+    joined.set_author(
+        name="KuliNet",
+        icon_url=bot.user.avatar.url
+    )
+    Error = discord.Embed(
+        title=f"Error!",
+        description="The bot doesn't know where to join make sure to join a channel first before execution of ```!join```",
+        timestamp=discord.utils.utcnow(),
+        color=discord.Color.green()
+    )
+    Error.set_author(
+        name="KuliNet",
+        icon_url=bot.user.avatar.url
+    )
+    if ctx.author.voice.channel is None:
+        await ctx.send(embed=Error)
+    else:
         await channel.connect()
         await ctx.send(embed=joined)
+
+
+
+
+bot.run(token=temptoken)
